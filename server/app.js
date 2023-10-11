224ea6082423bc3d47b2d290bd600cd47c6f2a79
unknown <feelingzty@gmail.com>
2023-10-10 23:07:34 -0700

    added get, put and delete

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 //create a new record
-
 app.post("/questions", async (req, res)=>{
     try {
         const {id, name, difficulty} = req.body;
@@ -18,6 +17,58 @@ app.post("/questions", async (req, res)=>{
             [id, name, difficulty]
         );
         res.json(newQuestion.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//get all records
+app.get("/questions", async (req, res) =>{
+    try {
+        const records = await pool.query("SELECT * FROM questions");
+        res.json(records.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//get a record
+app.get("/questions/:id", async (req, res)=>{
+    try {
+        const {id} = req.params;
+        const record = await pool.query("SELECT * FROM questions WHERE question_id = $1", [id]);
+        res.json(record.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//update a record
+app.put("/questions/:id", async (req, res)=>{
+    try {
+        const {id} = req.params;
+        const {question_name, difficulty} = req.body;
+        const updatedRecord = await pool.query(
+            "UPDATE questions SET question_name=$1, difficulty=$2 WHERE question_id=$3 RETURNING *",
+            [question_name, difficulty, id]
+        );
+        res.json(
+            {
+                message: "record is updated",
+                updatedRecord: updatedRecord.rows[0]
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//delete a record
+app.delete("/questions/:id", async (req, res)=>{
+    try {
+        const {id} = req.params;
+        await pool.query("DELETE FROM questions WHERE question_id=$1", [id]);
+        res.json("The question is successfully deleted");
     } catch (err) {
         console.error(err.message);
     }
