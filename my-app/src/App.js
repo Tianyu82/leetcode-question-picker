61016e69ea0146ae8f6df4fa3a116b5ab643f733
unknown <feelingzty@gmail.com>
2023-10-21 00:35:08 -0700

    Frontend: search: searching by question id on clicking the search button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import React, { Fragment } from 'react';
 
 import AddQuestions from './components/addQuestion.js';
 import Title from './components/title.js';
+import GetQuestions from './components/getQuestions';
 
 function App() {
   return (
     <Fragment>
       <Title />
       <AddQuestions />
+      <GetQuestions />
     </Fragment>
   );
 }
---
+++ my-app/src/components/getQuestions.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {useState, useEffect, Fragment} from 'react';
+
+
+const GetQuestions = () =>{
+    
+    const [searchText, setSearchText] = useState("");
+    const [questions, setQuestions] = useState([]);
+
+    const onSearchQuestion = async (searchText) =>{
+        try {
+            const response = await fetch(`http://localhost:5000/questions/${searchText}`);
+            const jsonData = await response.json();
+            const questionsArray = Array.isArray(jsonData) ? jsonData : [jsonData];
+            setQuestions(questionsArray);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    const handleInputChange = (e) =>{
+        setSearchText(e.target.value);
+    };
+
+    useEffect(() => {
+        onSearchQuestion(searchText);
+    }, []);
+
+    return (
+        <Fragment>
+            <div class="container mt-5 justify-content-center align-items-center">
+                <form>
+                    Search questions by id or name : <input type="search" placeholder="Search Here" value={searchText} onChange={handleInputChange} />
+                    <button type="button" class="ml-3" onClick={()=>onSearchQuestion(searchText)}>Search</button>
+                    <button type="button" class="ml-2">Clear</button>
+                </form>
+            </div>
+            
+            <table class="table mt-5 text-center">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Difficulty</th>
+                </tr>
+                </thead>
+                <tbody>
+                {questions.map(question=>(
+                    <tr>
+                        <td>{question.question_id}</td>
+                        <td>{question.question_name}</td>
+                        <td>{question.difficulty}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </Fragment>
+    );
+};
+
+export default GetQuestions;
+
+
